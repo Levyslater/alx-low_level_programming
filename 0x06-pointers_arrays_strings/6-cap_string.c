384f3f3758b4145ea8a7e9381dc61c313202e5a9
levslater <muthui@yandex.com>
2023-06-29 17:10:12 -0700

    strings and arrays

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,13 +8,11 @@
 
 char *cap_string(char *str)
 {
-	int index = 0;
-
-	while (str[index])
-	{
+int index = 0;
+while (str[index])
+{
 while (!(str[index] >= 'a' && str[index] <= 'z'))
 index++;
-
 if (str[index - 1] == ' ' ||
 str[index - 1] == '\t' ||
 str[index - 1] == '\n' ||
@@ -29,8 +27,8 @@ str[index - 1] == ')' ||
 str[index - 1] == '{' ||
 str[index - 1] == '}' ||
 index == 0)
-			str[index] -= 32;
-		index++;
-	}
-	return (str);
+str[index] -= 32;
+index++;
+}
+return (str);
 }
