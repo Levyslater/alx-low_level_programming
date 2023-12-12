c1dc8fc407a367be1eaa7614a59cdfde2fce9424
Levyslater <muthui@yandex.com>
2023-12-12 10:43:24 +0300

    list_length

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * dlistint_len - function that calculates the number of nodes
+ * @h: ponter to the first node
+ *
+ * Return: number of nodes
+*/
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+	dlistint_t *temp = h;
+
+	while (temp != NULL)
+	{
+		count++;
+		temp = temp->next;
+	}
+	return (count);
+}
