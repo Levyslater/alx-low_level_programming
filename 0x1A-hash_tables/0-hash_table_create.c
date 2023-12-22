c2d381caa44951da23d87b921c2d152c71f83e43
Levyslater <muthui@yandex.com>
2023-12-22 15:29:53 +0300

    create_hash_table

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - fuction creates a new struct table
+ * @size: defnes the table size
+ *
+ * Return: pointer to the new table, NULL otherwise
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned int i;
+	hash_table_t *ptr = malloc(sizeof(hash_table_t));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	ptr->size = size;
+	ptr->array = malloc(size * sizeof(hash_node_t *));
+
+	if (ptr->array == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+		ptr->array[i] = NULL;
+
+	return (ptr);
+}
