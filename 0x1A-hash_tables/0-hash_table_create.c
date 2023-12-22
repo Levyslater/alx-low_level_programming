d5dcbcb568ba0aa4bd0709e03a7e6dea7bac70d5
Levyslater <muthui@yandex.com>
2023-12-22 15:46:44 +0300

    create_hash_table2

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,8 +19,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 	ptr->array = (hash_node_t **)malloc(size * sizeof(hash_node_t *));
 
 	if (ptr->array == NULL)
+	{
 		free(ptr);
 		return (NULL);
+	}
 
 	for (i = 0; i < size; i++)
 		ptr->array[i] = NULL;
